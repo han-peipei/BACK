3fa2f42bada400f864590a33dbae0e20862108e4
han-peipei <1090874906@qq.com>
2023-12-01 20:01:48 +0800

    new file:   ICEDEPTHregrid_template.ncl 	modified:   namelist_template.input 	modified:   namelist_template.wps 	deleted:    slurm-2699637.out 	deleted:    test.ipynb 	deleted:    test.ncl 	deleted:    test.py 	deleted:    wrf-auto.sh 	modified:   wrf_auto.sh 	deleted:    wrf_auto1.sh 	deleted:    wrf_test.sh 	new file:   wrfout_icedepth_windsx_template.ncl

--- wrf_auto.sh
+++ wrf_auto.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 #: wrf model automation script
-#written by hpp
 
-export WRF_EM_CORE=1
-export WRF_NMM_CORE=0
-export WRFIO_NCD_LARGE_FILE_SUPPORT=1
-ulimit -s unlimited
+export LD_LIBRARY_PATH="/thfs1/software/loginnode/usr/lib/aarch64-linux-gnu/:/thfs1/software/netcdf/4.8.0-gcc9.3.0-mpi-x/lib/:/thfs1/software/hdf5/1.12.0-gcc9.3.0-mpi-x/lib/:/thfs1/software/hdf5/1.12.0-gcc9.3.0-mpi-x/lib/"
+export ESMF_DIR=/thfs1/software/spack/deb/liyueyan/linux-ubuntu20.04-aarch64/gcc-7.5.0/esmf-8.0.1-c2b4klc
+export PATH=$PATH:$ESMF_DIR/bin
+ulimit -c unlimited
 #########################################
 #            路径及起止时间              
 #########################################
 # BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-BASE_DIR=/thfs1/home/qx_hyt/hpp/code/
-GFS_DIR=/thfs1/home/qx_hyt/hpp/data/GFS/2023051412/
-WPS_GEOG=/thfs1/home/qx_hyt/hpp/model/PWRF/WPS-4.4/WPS_GEOG               
-WPS_DIR=/thfs1/home/qx_hyt/hpp/model/PWRF/WPS-4.4/ 
-WRF_DIR=/thfs1/home/qx_hyt/hpp/model/PWRF/WRF-4.3.3/run/    
-start_time="2023-05-14_12:00:00"
-end_time="2023-05-18_00:00:00"
+# end_time=$(date -d yesterday +%Y-%m-%d)"_18:00:00"
+# start_time="2023-05-14_12:00:00"
+# end_time="2023-05-18_00:00:00"
+start_time=$(date -d yesterday +%Y-%m-%d)"_12:00:00"
+end_time=$(date -d yesterday +%Y-%m-%d --date="+3 day")"_00:00:00"
 run_hours=84
+BASE_DIR=/thfs1/home/qx_hyt/PWRF/hpp/wrf_auto
+GFS_DIR=/thfs1/software/WRFV4.0DATA/GFS/0p50/gfs.$(date -d yesterday +%Y%m%d)"12"/
+WPS_GEOG=/thfs1/home/qx_hyt/hpp/model/PWRF/WPS-4.4/WPS_GEOG               
+WPS_DIR=/thfs1/home/qx_hyt/PWRF/WPS-4.4/ 
+WRF_DIR=/thfs1/home/qx_hyt/PWRF/WRF-4.3.3/run/ 
+ncl=/thfs1/software/spack/deb/liyueyan/linux-ubuntu20.04-aarch64/gcc-7.5.0/ncl-6.6.2-53z6sd4/bin 
+set -x; exec 2>$BASE_DIR/logfile
 # #########################################
 # #             Running wps  
 # #########################################
@@ -40,6 +44,7 @@ declare -A patterns
 patterns["INPUT_START_DATE"]=$start_time
 patterns["INPUT_END_DATE"]=$end_time
 patterns["GEOG_DATA"]=$WPS_GEOG
+patterns["PREFIX"]='FILE'
 patterns["METGRID_PATH"]=${WPS_DIR}wps_output
 sed_cmd=""
 for key in "${!patterns[@]}"; do
@@ -52,22 +57,22 @@ output_file="namelist.wps"
 sed $sed_cmd < "$input_file" > "$output_file"
 
 ### 运行 wps
+cd $WPS_DIR && rm FILE:* geo_em* ICEDEPTH:* SST:* PFILE:* GRIBFILE* metgrid.log.0* geogrid.log.0* slurm-* 
+
 ./link_grib.csh ${GFS_DIR}gfs* 
 ln -sf ungrib/Variable_Tables/Vtable.GFS Vtable
 
-cd $WPS_DIR && rm FILE:* geo_em*
-
  cat > geogrid.sh <<EOF
 #!/bin/bash
-yhrun -N 1 -n 56 -p thcp1 ./geogrid.exe >&geogrid.out
+yhrun -N 1 -n 60 -p thcp1 ./geogrid.exe >&geogrid.out
 EOF
 chmod a+x geogrid.sh
-yhbatch -p thcp1 $WPS_DIR/geogrid.sh >& jobID_geogrid
+yhbatch -N 1 -n 60 -p thcp1 $WPS_DIR/geogrid.sh >& jobID_geogrid
 pid_geogrid=$(awk '{print $4}' jobID_geogrid)
 while true; do
     job_status=$(/usr/bin/yhqueue | grep "$pid_geogrid" | awk '{print $1}')
     if [ "$job_status" == "$pid_geogrid" ]; then
-        sleep 60  # 作业完成时退出循环
+        sleep 100  
     else
         break  # 作业完成或其他状态时退出循环
     fi
@@ -79,48 +84,93 @@ yhrun -N 1 -n 1 -p thcp1 ./ungrib.exe >&ungrib.out
 EOF
 chmod a+x ungrib.sh
 yhbatch -p thcp1 $WPS_DIR/ungrib.sh >& jobID_ungrib
-pid_ungrib=$(awk '{print $4}' jobID_ungrib)
+pid_ungrib_1=$(awk '{print $4}' jobID_ungrib)
 while true; do
-    job_status=$(/usr/bin/yhqueue | grep "$pid_ungrib" | awk '{print $1}')
-    if [ "$job_status" == "$pid_ungrib" ]; then
-        sleep 60 
+    job_status=$(/usr/bin/yhqueue | grep "$pid_ungrib_1" | awk '{print $1}')
+    if [ "$job_status" == "$pid_ungrib_1" ]; then
+        sleep 100 
     else
         break  
     fi
 done
+### 根据 namelist_template.wps 编辑 namelist.wps
+declare -A patterns
+patterns["PREFIX"]='SST'
+sed_cmd=""
+for key in "${!patterns[@]}"; do
+  old_pattern="$key"
+  new_pattern="${patterns[$key]}"
+  sed_cmd+=" -e s|$old_pattern|$new_pattern|g"
+done
+input_file=${BASE_DIR}/namelist_template.wps
+output_file="namelist.wps"
+sed $sed_cmd < "$input_file" > "$output_file"
+
+yhbatch -p thcp1 $WPS_DIR/ungrib.sh >& jobID_ungrib
+pid_ungrib_2=$(awk '{print $4}' jobID_ungrib)
+while true; do
+    job_status=$(/usr/bin/yhqueue | grep "$pid_ungrib_2" | awk '{print $1}')
+    if [ "$job_status" == "$pid_ungrib_2" ]; then
+        sleep 100 
+    else
+        break  
+    fi
+done
+cd $WPS_DIR 
+### 根据 ICEDEPTHregrid_template.ncl 编辑 ICEDEPTHregrid.ncl
+declare -A patterns
+patterns["MONTH"]=${start_time:5:2}
+patterns["HEFF"]=heff${start_time:0:4}${start_time:5:2}.H2023
+patterns["DATE_BEG"]=${start_time:0:4}${start_time:5:2}${start_time:8:2}${start_time:11:2}
+
+sed_cmd=""
+for key in "${!patterns[@]}"; do
+  old_pattern="$key"
+  new_pattern="${patterns[$key]}"
+  sed_cmd+=" -e s|$old_pattern|$new_pattern|g"
+done
+input_file=${BASE_DIR}/ICEDEPTHregrid_template.ncl
+output_file="ICEDEPTHregrid.ncl"
+sed $sed_cmd < "$input_file" > "$output_file"
+
+cd $WPS_DIR 
+for ((i=0; i<=run_hours/3; i++)); do
+  $ncl/ncl -Q ind_date=$i ./ICEDEPTHregrid.ncl
+done
+#&& ncl ICEDEPTHregrid.ncl >ice.log
 
  cat > metgrid.sh <<EOF
 #!/bin/bash
-yhrun -N 1 -n 1 -p thcp1 ./metgrid.exe >&metgrid.out
+yhrun -N 1 -n 60 -p thcp1 ./metgrid.exe >&metgrid.out
 EOF
 chmod a+x metgrid.sh
-yhbatch -p thcp1 metgrid.sh  >& jobID_megrid
+yhbatch -N 1 -n 60 -p thcp1 metgrid.sh  >& jobID_megrid
 pid_metgrid=$(awk '{print $4}' jobID_megrid)
 while true; do
     job_status=$(/usr/bin/yhqueue | grep "$pid_metgrid" | awk '{print $1}')
     if [ "$job_status" == "$pid_metgrid" ]; then
-        sleep 60  
+        sleep 100  
     else
         break 
     fi
 done
 
-# #########################################
-# #             Running wrf  
-# #########################################
-cd $WRF_DIR
+#########################################
+#             Running wrf  
+#########################################
+cd $BASE_DIR
+
 ### 新建文件夹存放 wrf 输出
-if [ -d wrf_output  ];then
-  rm -rf wrf_output
+if [ -d ${start_time:0:4}{start_time:5:2}{start_time:8:2}  ];then
+  rm -rf ${start_time:0:4}${start_time:5:2}${start_time:8:2}
 else
-  echo wrf_output no exist
+  echo ${start_time:0:4}${start_time:5:2}${start_time:8:2} no exist
 fi
 
-mkdir wrf_output
-cd wrf_output 
-cd ..
+mkdir ${start_time:0:4}${start_time:5:2}${start_time:8:2}
+cd $WRF_DIR && rm wrfbdy* wrfinput* wrflowinp*
 
-### 根据 namelist_template.input 编辑 namelist.input
+## 根据 namelist_template.input 编辑 namelist.input
 declare -A patterns
 patterns["RUN_HOURS"]=$run_hours
 patterns["START_YEAR_1"]=${start_time:0:4}
@@ -131,7 +181,7 @@ patterns["END_YEAR_1"]=${end_time:0:4}
 patterns["END_MONTH_1"]=${end_time:5:2}
 patterns["END_DAY_1"]=${end_time:8:2}
 patterns["END_HOUR_1"]=${end_time:11:2}
-patterns["OUTNAME"]="${WRF_DIR}wrf_output/wrfout_d<domain>_<date>"
+patterns["OUTNAME"]="${BASE_DIR}/${start_time:0:4}${start_time:5:2}${start_time:8:2}/wrfout_d<domain>_<date>"
 
 sed_cmd="s|<pattern_to_replace_1>|<replacement_text_1>|g"
 for key in "${!patterns[@]}"; do
@@ -144,19 +194,20 @@ output_file="namelist.input"
 sed "$sed_cmd" < "$input_file" > "$output_file"
 
 ### 运行 wrf
-cd $WRF_DIR && rm met* && ln -sf ${WPS_DIR}wps_output/met* .
+cd $WRF_DIR && rm met* 
+ln -sf ${WPS_DIR}wps_output/met* .
 
 cat > real.sh <<EOF
 #!/bin/bash
-yhrun -N 1 -n 1 -p thcp1 ./real.exe >&real.out
+yhrun -N 1 -n 60 -p thcp1 ./real.exe >&real.out
 EOF
 chmod a+x real.sh
-yhbatch -p thcp1 real.sh  >& jobID_real
+yhbatch -N 1 -n 60 -p thcp1 real.sh  >& jobID_real
 pid_real=$(awk '{print $4}' jobID_real)
 while true; do
     job_status=$(/usr/bin/yhqueue | grep "$pid_real" | awk '{print $1}')
     if [ "$job_status" == "$pid_real" ]; then
-        sleep 60  
+        sleep 100  
     else
         break 
     fi
@@ -164,16 +215,57 @@ done
 
 cat > wrf.sh <<EOF
 #!/bin/bash
-yhrun -N 1 -n 64 -p thcp1 ./wrf.exe >&wrf.out
+yhrun -N 2 -n 81 -p thcp1 ./wrf.exe >&wrf.out
 EOF
 chmod a+x wrf.sh
-yhbatch -p thcp1 wrf.sh  >& jobID_wrf
+yhbatch -N 2 -n 81 -p thcp1 wrf.sh  >& jobID_wrf
 pid_wrf=$(awk '{print $4}' jobID_wrf)
 while true; do
     job_status=$(/usr/bin/yhqueue | grep "$pid_wrf" | awk '{print $1}')
     if [ "$job_status" == "$pid_wrf" ]; then
-        sleep 60  
+        sleep 300  
     else
         break 
     fi
 done
+
+#########################################
+#                后处理  
+#########################################
+cd $BASE_DIR
+
+declare -A patterns
+patterns["DIR"]=${BASE_DIR}/${start_time:0:4}${start_time:5:2}${start_time:8:2}/
+patterns["FILENAME"]=wrfout_d01_${start_time:0:4}-${start_time:5:2}-${start_time:8:2}_12:00:00
+patterns["OUTPUT"]=${BASE_DIR}/${start_time:0:4}${start_time:5:2}${start_time:8:2}/wrfout_icedepth_windsx
+
+sed_cmd="s|<pattern_to_replace_1>|<replacement_text_1>|g"
+for key in "${!patterns[@]}"; do
+  old_pattern="$key"
+  new_pattern="${patterns[$key]}"
+  sed_cmd+=";s|$old_pattern|$new_pattern|g"
+done
+input_file=${BASE_DIR}/wrfout_icedepth_windsx_template.ncl
+output_file="wrfout_icedepth_windsx.ncl"
+sed "$sed_cmd" < "$input_file" > "$output_file"
+
+ cat > ncl.sh <<EOF
+#!/bin/bash
+yhrun -N 1 -n 1 -p thcp1 $ncl/ncl wrfout_icedepth_windsx.ncl >wind.log
+EOF
+chmod a+x ncl.sh
+yhbatch -N 1 -n 1 -p thcp1 $BASE_DIR/ncl.sh >& jobID_ncl_wind
+pid_ncl=$(awk '{print $4}' jobID_ncl_wind)
+while true; do
+    job_status=$(/usr/bin/yhqueue | grep "$pid_ncl" | awk '{print $1}')
+    if [ "$job_status" == "$pid_ncl" ]; then
+        sleep 100  
+    else
+        break  # 作业完成或其他状态时退出循环
+    fi
+done
+
+#########################################
+#                删除日志  
+#########################################
+# cd $WPS_DIR && rm metgrid.log.0* geogrid.log.0* slurm-* 
